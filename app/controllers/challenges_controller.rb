67cde7fd8d35670afe42424a69452f9987cdbd83
mayth <chimeaquas@hotmail.com>
2014-03-18 08:33:53 +0900

    Add comments (routing).

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,10 +1,13 @@
 class ChallengesController < ApplicationController
+  # /challenges
   def index
   end
 
+  # /challenges/:id
   def show
   end
 
+  # /challenges/:id/answer
   def answer
   end
 end
