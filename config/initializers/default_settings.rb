985ef2bd11ba47dad84237e4e280fdcef9def424
mayth <chimeaquas@hotmail.com>
2014-03-30 04:01:36 +0900

    Rename settings name.

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,5 +1,5 @@
 Setting.save_default(:ctf_name, 'Parasol')
-Setting.save_default('submit_burst.count', 10)
-Setting.save_default('submit_burst.time', 10)
+Setting.save_default(:submit_burst_count, 10)
+Setting.save_default(:submit_burst_time, 10)
 Setting.save_default(:cooling_down, 900)
 Setting.save_default(:rules, '**No rules, No world.**')
