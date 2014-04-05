51f510a6147ad9a3976c807f448f6e351fdca0a6
mayth <chimeaquas@hotmail.com>
2014-04-06 00:12:40 +0900

    No longer use migrant for Admin.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,4 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  no_structure  # satisfies migrant...
 end
