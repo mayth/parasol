db1078239108cde327cc7c9fb1a6de4ab521f3df
mayth <chimeaquas@hotmail.com>
2014-04-06 04:42:07 +0900

    Order posts by updated date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @challenges = Challenge.opened
-    @posts = player_signed_in? ? Post.all : Post.public_only
+    @posts =
+      (player_signed_in? ? Post.all : Post.public_only)
+      .order(updated_at: :desc)
   end
 
   def rules
