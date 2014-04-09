7c4e040fa3ea335db96fceca4aa983b04a9f9a56
mayth <chimeaquas@hotmail.com>
2014-04-10 00:06:03 +0900

    Add stub for ApplicationHelper spec.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ChallengesHelper. For example:
+#
+# describe ChallengesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+describe ApplicationHelper do
+  describe 'within_team_registration_period?' do
+  end
+
+  describe 'within_contest_period?' do
+  end
+
+  describe 'markdown_render' do
+  end
+end
