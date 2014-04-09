90600436a228745584e418080b233ba3ae3b1d73
mayth <chimeaquas@hotmail.com>
2014-04-09 23:20:39 +0900

    Satisfy rubocop.

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -12,7 +12,7 @@ describe ChallengesController do
   end
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it 'returns http success' do
       sign_in player
       get 'index'
       expect(response).to be_success
@@ -20,7 +20,7 @@ describe ChallengesController do
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
+    it 'returns http success' do
       sign_in player
       get 'show', id: @challenge.id
       expect(response).to be_success
@@ -29,9 +29,9 @@ describe ChallengesController do
 
   describe "POST 'answer'" do
     context 'with valid parameter' do
-      it "returns http success" do
+      it 'returns http success' do
         sign_in player
-        post 'answer', id: @challenge.id, answer: {answer: 'FLAG_SPEC'}
+        post 'answer', id: @challenge.id, answer: { answer: 'FLAG_SPEC' }
         expect(response).to redirect_to challenge_path(@challenge)
       end
     end
