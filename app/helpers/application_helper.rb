8ad32c43f17c807b0c2796bd5712f3cb58efe317
mayth <chimeaquas@hotmail.com>
2014-04-10 00:24:51 +0900

    within_team_registration_period? & within_contest_period? accepts parameter.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
-  def within_team_registration_period?
-    Time.zone.now.between?(
+  def within_team_registration_period?(time = nil)
+    t = time || Time.zone.now
+    t.between?(
       Setting.team_registrable_from, Setting.team_registrable_until
     )
   end
 
-  def within_contest_period?
-    Time.zone.now.between?(
+  def within_contest_period?(time = nil)
+    t = time || Time.zone.now
+    t.between?(
       Setting.contest_starts_at, Setting.contest_ends_at
     )
   end
