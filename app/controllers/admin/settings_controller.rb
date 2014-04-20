4dae24852b5caaf952e9a966067840cae28a82ca
mayth <chimeaquas@hotmail.com>
2014-04-20 13:40:30 +0900

    [re Fix #9] Parse settings value on update

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -7,8 +7,17 @@ class Admin::SettingsController < ApplicationController
 
   # PATCH/PUT /admin/settings
   def update
+    converter = {
+      submit_burst_count: -> (s) { s.to_i },
+      submit_burst_time:  -> (s) { s.to_i },
+      cooling_down:       -> (s) { s.to_i },
+      team_registrable_from:  -> (s) { Time.parse(s) },
+      team_registrable_until: -> (s) { Time.parse(s) },
+      contest_opens_at: -> (s) { Time.parse(s) },
+      contest_ends_at:  -> (s) { Time.parse(s) }
+    }
     settings_params.each do |k, v|
-      Setting[k] = v
+      Setting[k] = converter.key?(k.to_sym) ? converter[k.to_sym].call(v) : v
     end
     respond_to do |format|
       format.html { redirect_to admin_settings_path, notice: 'Settings saved.' }
